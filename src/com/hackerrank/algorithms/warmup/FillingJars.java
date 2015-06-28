42d292de806a73df30b492a36f445f8945e2e14c
Mohammed Aaqib Ansari <maaquib@gmail.com>
2015-06-28 18:24:05 -0400

    Filling jars

---
+++ src/com/hackerrank/algorithms/warmup/FillingJars.java
@@ -0,0 +1,21 @@
+package com.hackerrank.algorithms.warmup;
+
+import java.util.Scanner;
+
+public class FillingJars {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long jars = sc.nextInt();
+		long operations = sc.nextInt();
+		long sum = 0;
+		for (int i = 0; i < operations; i++) {
+			long a = sc.nextInt();
+			long b = sc.nextInt();
+			long k = sc.nextInt();
+			sum += k * (Math.abs(b - a) + 1);
+		}
+		System.out.println(sum / jars);
+		sc.close();
+	}
+}
