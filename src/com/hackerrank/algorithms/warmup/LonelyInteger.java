d943c36e69b4acd9e9faf70948098cc4faaa3585
Mohammed Aaqib Ansari <maaquib@gmail.com>
2015-06-29 09:44:43 -0400

    LonelyInteger

---
+++ src/com/hackerrank/algorithms/warmup/LonelyInteger.java
@@ -0,0 +1,27 @@
+package com.hackerrank.algorithms.warmup;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class LonelyInteger {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCases = sc.nextInt();
+		ArrayList<Integer> in = new ArrayList<Integer>();
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		for (int i = 0; i < testCases; i++) {
+			int num = sc.nextInt();
+			if (in.contains(num) && !temp.contains(num)) {
+				temp.add(num);
+			} else {
+				in.add(num);
+			}
+		}
+		in.removeAll(temp);
+		for (Integer max : in) {
+			System.out.println(max);
+		}
+		sc.close();
+	}
+}
