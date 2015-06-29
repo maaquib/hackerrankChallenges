20bab0da760b726f2905a3062844b9ca57fcb50f
Mohammed Aaqib Ansari <maaquib@gmail.com>
2015-06-29 10:06:45 -0400

    HalloweenParty

---
+++ src/com/hackerrank/algorithms/warmup/HalloweenParty.java
@@ -0,0 +1,21 @@
+package com.hackerrank.algorithms.warmup;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HalloweenParty {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCases = sc.nextInt();
+		ArrayList<Long> out = new ArrayList<Long>();
+		for (int i = 0; i < testCases; i++) {
+			long num = sc.nextLong();
+			out.add((num / 2) * (num - (num / 2)));
+		}
+		for (Long max : out) {
+			System.out.println(max);
+		}
+		sc.close();
+	}
+}
