14369f4d6e0983b9078dc63cb8f8d0584a2e2ab0
Mohammed Aaqib Ansari <maaquib@gmail.com>
2015-07-01 14:44:26 -0400

    Project Euler Problem 4

--- projectEuler/problem1.rb
+++ projectEuler/problem1.rb
@@ -1,9 +1,10 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
 def sum (num, div)
 	return div * (num/div * (num/div + 1)) / 2
 end
 
-cases = gets.chomp.to_i
-cases.times do
-	numUpto = gets.chomp.to_i
-	puts sum((numUpto-1), 5) + sum((numUpto-1), 3) - sum((numUpto-1), 15)
-end
+numUpto = gets.chomp.to_i
+puts sum((numUpto-1), 5) + sum((numUpto-1), 3) - sum((numUpto-1), 15)
